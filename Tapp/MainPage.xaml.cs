ca96a207d7883f0bd27b5eb1e3aae8491f8bbff5
bencarpena <bencarpena@gmail.com>
2018-11-14 22:24:24 -0600

    Initial check-in of module Tapp

---
+++ Tapp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tapp
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                DisplayAlert("Alert", "Go where?", "Huh?", "Nvm");
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException();
+            }
+                
+
+
+        }
+
+
+    }
+}
