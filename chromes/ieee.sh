c551d9df81e16e5ab0cdccd5b0039b834bc35fbf
Benjamin Carpena <bencarpena@Helios.local>
2021-02-27 12:36:39 -0600

    20210227_123639

--- chromes/ieee.sh
+++ chromes/ieee.sh
@@ -12,3 +12,4 @@ open -a "Google Chrome" https://www.ieee.org/
 open -a "Google Chrome" https://standards.ieee.org/project/802_3cu.html
 open -a "Google Chrome" https://development.standards.ieee.org/myproject-web/app#managepars
 open -a "Google Chrome" https://console.openearth.community/users/edit
+open -a "Google Chrome" https://quantum.ieee.org/education/workforce-development#2021-05 #quantum
