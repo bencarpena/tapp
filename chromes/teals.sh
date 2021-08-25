f22db59ebf774f954884bcf410b40420459ca279
Benjamin Carpena <bencarpena@Helios.local>
2021-08-25 17:55:31 -0500

    20210825_175531

--- chromes/teals.sh
+++ chromes/teals.sh
@@ -24,6 +24,13 @@ open -a "Google Chrome" https://makeymakey.com/
 open -a "Google Chrome" https://docs.google.com/document/d/1zNciZwoCQbG31K-4VQzPv6XZpZjsWLGy3HrUWSr-Hxk/edit #earsketch
 open -a "Google Chrome" https://classkick.com/
 
+# Code visualizers
+open -a "Google Chrome" https://pythontutor.com/visualize.html#mode=edit
+open -a "Google Chrome" https://www.jdoodle.com/online-java-compiler/
+
+# Teaching utilities (Java)
+open -a "Google Chrome" https://csawesome.runestone.academy/runestone/books/published/csawesome/index.html
+open -a "Google Chrome" https://sites.google.com/view/csawesome/teacher-materials/unit-1?authuser=0
 
 # SEFH Houston 
 open -a "Google Chrome" https://sefhouston.org/for-participants/ 
